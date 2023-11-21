f7731b72918a7d6c69b6264c23b3d7f66343957f
tungdd <tungdd98@gmail.com>
2023-11-21 22:53:37 +0700

    Add basic e2e

--- apps/basic-e2e/src/example.spec.ts
+++ apps/basic-e2e/src/example.spec.ts
@@ -12,3 +12,31 @@ test('Verify that the "login-button" is inactive', async ({ page }) => {
 
   await expect(elm).toBeDisabled();
 });
+
+test('Set "admin" to "login-input-email"', async ({ page }) => {
+  await page.goto('/');
+  const inputEmail = page.getByTestId('login-input-email');
+  const errorEmail = page.getByTestId('login-input-email-error');
+  const loginBtn = page.getByTestId('login-button');
+  await inputEmail.fill('admin');
+
+  await expect(errorEmail).toBeVisible();
+  await expect(loginBtn).toBeDisabled();
+});
+
+test('Set "admin@gmail.com" to "login-input-email" and Set "123" to "login-input-password"', async ({
+  page,
+}) => {
+  await page.goto('/');
+  const inputEmail = page.getByTestId('login-input-email');
+  const errorEmail = page.getByTestId('login-input-email-error');
+  const inputPassword = page.getByTestId('login-input-password');
+  const errorPassword = page.getByTestId('login-input-password-error');
+  const loginBtn = page.getByTestId('login-button');
+  await inputEmail.fill('admin@gmail.com');
+  await inputPassword.fill('123');
+
+  await expect(errorEmail).toBeHidden();
+  await expect(errorPassword).toBeHidden();
+  await expect(loginBtn).toBeEnabled();
+});
